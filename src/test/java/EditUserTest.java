da0a7836267b423e46677f5b8e0b5b0e627dd867
Ermak0vYurii <grantovod1@gmail.com>
2025-01-31 17:48:35 +0500

    Готовы тесты на изменение данных пользователя

--- src/test/java/EditUserTest.java
+++ src/test/java/EditUserTest.java
@@ -37,6 +37,22 @@ public class EditUserTest {
     }
 
     @Test
+    @DisplayName("Изменение email на email, который уже используется")
+    public void failEditEmailTest() {
+        User newUser = new User("newUser@ya.ru", "pass", "Name");
+        UserLogin newUserLogin = new UserLogin(newUser.getEmail(), newUser.getPassword());
+        Response forToken = client.createUser(newUser);
+        client.loginUser(login);
+        User editEmail = new User(newUser.getEmail(), null, null);
+        Response response = client.editUser(editEmail, token);
+        client.compareStatusCode(response, SC_FORBIDDEN);
+        client.compareResponseBodyMessage(response, "User with such email already exists");
+        String tokenNewUser = client.getToken(forToken);
+        client.loginUser(newUserLogin);
+        client.deleteUser(tokenNewUser);
+    }
+
+    @Test
     @DisplayName("Изменение name авторизованного пользователя")
     public void editNameAuthUserTest() {
         client.loginUser(login);
@@ -54,5 +70,14 @@ public class EditUserTest {
         Response response = client.editUser(editPasswordUser, token);
         client.compareStatusCode(response, SC_OK);
         client.compareResponseBody(response);
+    }
+
+    @Test
+    @DisplayName("Изменение данных неавторизованного пользователя")
+    public void editUserWithoutAuthTest() {
+        User editUser = new User("updateemail@ya.ru", "newpassword", "Alex");
+        Response response = client.editUser(editUser);
+        client.compareStatusCode(response, SC_UNAUTHORIZED);
+        client.compareResponseBodyMessage(response, "You should be authorised");
     }
 }
