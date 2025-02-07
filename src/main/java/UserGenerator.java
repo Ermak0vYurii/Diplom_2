25ba5690c23ae5e1a5ff2626eaff944e8cc439db
Ermak0vYurii <grantovod1@gmail.com>
2025-02-07 13:48:59 +0500

    Устранение замечаний

--- src/main/java/UserGenerator.java
+++ src/main/java/UserGenerator.java
@@ -5,7 +5,7 @@ public class UserGenerator {
 
     public static User getRandomUser() {
         Faker faker = new Faker();
-        String email = faker.internet().emailAddress();;
+        String email = faker.internet().emailAddress();
         String password = faker.internet().password();
         String name = faker.name().firstName();
         return new User(email, password, name);
